569d558fcd6fdf1365afb0a57d26445ff9869c68
Julia Signell <jsignell@gmail.com>
2020-02-18 15:35:22 -0500

    Fix CI: use linux miniconda

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -2,7 +2,7 @@ set -xe
 
 #!/usr/bin/env bash
 # Install miniconda
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 export BOTO_CONFIG=/dev/null
