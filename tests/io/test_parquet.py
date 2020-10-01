354e71b6fd61a1f268dfc0c19a499d153d1a7c30
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-10-01 15:01:28 +0200

    BUG: fix column selection pushdown with parquet reader

--- tests/io/test_parquet.py
+++ tests/io/test_parquet.py
@@ -1,11 +1,17 @@
 import geopandas
 import dask_geopandas
+import dask.dataframe as dd
 
 import pytest
 from geopandas.testing import assert_geodataframe_equal
 
 
 pa = pytest.importorskip("pyarrow")
+
+
+pytestmark = pytest.mark.filterwarnings(
+    "ignore:this is an initial implementation:UserWarning"
+)
 
 
 def test_parquet_roundtrip(tmp_path):
@@ -34,3 +40,22 @@ def test_parquet_roundtrip(tmp_path):
     result_part0 = geopandas.read_parquet(basedir / "part.0.parquet")
     result_part0.index.name = None
     assert_geodataframe_equal(result_part0, df.iloc[:45])
+
+
+def test_column_selection_push_down(tmp_path):
+    # set up dataset
+    df = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
+    ddf = dask_geopandas.from_geopandas(df, npartitions=4)
+    basedir = tmp_path / "dataset"
+    ddf.to_parquet(basedir)
+
+    ddf = dask_geopandas.read_parquet(basedir)
+
+    # selecting columns including geometry column still gives GeoDataFrame
+    ddf_subset = ddf[["pop_est", "geometry"]]
+    assert type(ddf_subset) is dask_geopandas.GeoDataFrame
+
+    # selecting a single non-geometry column on the dataframe should work
+    s = ddf["pop_est"]
+    assert type(s) is dd.Series
+    assert s.max().compute() == df["pop_est"].max()
