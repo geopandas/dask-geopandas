aeb0b6082510db83100b9ad5840733a826e2c4fb
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-10-10 18:20:37 +0200

    BUG: fix column selection pushdown with parquet reader (#23)

--- dask_geopandas/io/parquet.py
+++ dask_geopandas/io/parquet.py
@@ -39,7 +39,19 @@ class GeoArrowEngine(ArrowEngine):
         _kwargs.update({"use_threads": False, "ignore_metadata": False})
 
         # TODO support additional keywords
-        return _arrow_to_geopandas(arrow_table)
+        try:
+            return _arrow_to_geopandas(arrow_table)
+        except ValueError as err:
+            # when no geometry column is selected, the above will error.
+            # We want to fallback to reading it as a plain dask object, because
+            # the column selection can be an automatic pushdown (eg `ddf['col']`)
+            # TODO more robust detection of when to fall back?
+            if "No geometry columns are included" in str(err):
+                return super()._arrow_table_to_pandas(
+                    arrow_table, categories=categories, **kwargs
+                )
+            else:
+                raise
 
     @classmethod
     def _pandas_to_arrow_table(
