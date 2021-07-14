b86b5d29af3f0f8ac8816458ea825ae8e0f896e9
tastatham <tastatham@gmail.com>
2021-07-14 11:31:09 +0100

    add hilbert_curve to CI env & fix test error

--- tests/test_hilbert_curve.py
+++ tests/test_hilbert_curve.py
@@ -2,22 +2,8 @@ import pytest
 import pandas as pd
 from hilbertcurve.hilbertcurve import HilbertCurve
 from dask_geopandas.hilbert_distance import _continuous_to_discrete_coords
-from dask_geopandas import from_geopandas, hilbert_distance
+from dask_geopandas import from_geopandas
 from .test_core import geoseries_points, geoseries_lines, geoseries_polygons
-
-@pytest.fixture
-def geoseries_points(geoseries_points):
-	return geoseries_points
-
-
-@pytest.fixture
-def geoseries_lines(geoseries_lines):
-	return geoseries_lines
-
-
-@pytest.fixture
-def geoseries1(geoseries_polygons):
-	return geoseries_polygons
 
 
 @pytest.fixture(params=[geoseries_points, geoseries_lines, geoseries_polygons])
@@ -27,16 +13,16 @@ def geom_types(request):
 
 @pytest.fixture
 def test_hilbert_distance_dask(geom_types):
-    
-    bounds = geoseries.bounds.to_numpy()
-    total_bounds = geoseries.total_bounds
+
+    bounds = geom_types.bounds.to_numpy()
+    total_bounds = geom_types.total_bounds
     coords = _continuous_to_discrete_coords(total_bounds, bounds, p=15)
-    
+
     hilbert_curve = HilbertCurve(p=15, n=2)
     expected = hilbert_curve.distances_from_points(coords)
 
-    ddf = from_geopandas(geoseries, npartitions=1)
+    ddf = from_geopandas(geom_types, npartitions=1)
     result = ddf.hilbert_distance().compute()
-    
+
     assert list(result) == expected
     assert isinstance(result, pd.Series)
