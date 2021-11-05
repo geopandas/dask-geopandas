2c7c6a7526b6c85f72ca243f33f0ed82a6e249cf
bernardpazio <bernardpazio@gmail.com>
2021-11-05 13:08:49 +0000

    BUG: do not copy spatial partitions when None (#121)

--- tests/test_core.py
+++ tests/test_core.py
@@ -447,6 +447,13 @@ def test_map_partitions_get_geometry(geodf_points):
     assert_geoseries_equal(result, expected)
 
 
+def test_copy_none_spatial_partitions(geoseries_points):
+    ddf = dask_geopandas.from_geopandas(geoseries_points, npartitions=2)
+    ddf.spatial_partitions = None
+    ddf_copy = ddf.copy()
+    assert ddf_copy.spatial_partitions is None
+
+
 class TestDissolve:
     def setup_method(self):
         self.world = geopandas.read_file(
