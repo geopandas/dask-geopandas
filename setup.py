033889f2d864b9b4ced99929fa1893915dcbe1a3
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-02-19 13:36:50 +0100

    update setup.py to work with non-legacy setuptool.build_meta

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import os
+import sys
+
 from setuptools import setup, find_packages
+
+# ensure the current directory is on sys.path so versioneer can be imported
+# when pip uses PEP 517/518 build rules.
+# https://github.com/python-versioneer/python-versioneer/issues/193
+sys.path.append(os.path.dirname(__file__))
 
 import versioneer
 
