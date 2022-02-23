0c5d9d8d8044b160694a977c38c35b81b8aae0d2
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-02-23 22:02:53 +0100

    ENH: enable to_parquet with partition_on with attribute column (#171)

--- tests/io/test_parquet.py
+++ tests/io/test_parquet.py
@@ -170,3 +170,32 @@ def test_parquet_empty_dataset(tmp_path):
     # will raise in not finding any geo metadata)
     with pytest.raises(ValueError, match="No dataset parts discovered"):
         dask_geopandas.read_parquet(tmp_path / "data.*.parquet")
+
+
+@pytest.mark.skipif(
+    not Version(dask.__version__) > Version("2022.02.0"),
+    reason="Only works with dask 2022.02.1 or up",
+)
+def test_parquet_partition_on(tmp_path):
+    df = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
+    ddf = dask_geopandas.from_geopandas(df, npartitions=4)
+
+    # Writing a partitioned dataset based on one of the attribute columns
+    basedir = tmp_path / "naturalearth_lowres_by_continent.parquet"
+    ddf.to_parquet(basedir, partition_on="continent")
+
+    # Check for one of the partitions that the file is present and is correct
+    assert len(list(basedir.iterdir())) == 10  # 8 continents + 2 metadata files
+    assert (basedir / "continent=Africa").exists()
+    result_africa = geopandas.read_parquet(basedir / "continent=Africa")
+    expected = df[df["continent"] == "Africa"].drop(columns=["continent"])
+    result_africa.index.name = None
+    assert_geodataframe_equal(result_africa, expected)
+
+    # Check roundtrip
+    result = dask_geopandas.read_parquet(basedir)
+    assert result.npartitions >= 8
+    assert result.spatial_partitions is not None
+    expected = df.copy()
+    expected["continent"] = expected["continent"].astype("category")
+    assert_geodataframe_equal(result.compute(), expected, check_like=True)
