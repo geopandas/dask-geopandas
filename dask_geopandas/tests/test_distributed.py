f4a37e017aed9a8ec2209801a2bef025e3a03f2e
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-06-24 15:09:30 +0200

    cleanup

--- dask_geopandas/tests/test_distributed.py
+++ dask_geopandas/tests/test_distributed.py
@@ -12,8 +12,6 @@ distributed = pytest.importorskip("distributed")
 
 from distributed import Client, LocalCluster
 
-# from distributed.utils_test import gen_cluster
-
 
 @pytest.mark.skipif(
     Version(distributed.__version__) < Version("2024.6.0"),
@@ -38,15 +36,3 @@ def test_spatial_shuffle(naturalearth_cities):
 
     expected = df_points.sort_values("geometry").reset_index(drop=True)
     assert_geodataframe_equal(result.reset_index(drop=True), expected)
-
-
-# @gen_cluster(client=True)
-# async def test_spatial_shuffle(c, s, a, b, naturalearth_cities):
-#     df_points = geopandas.read_file(naturalearth_cities)
-#     ddf_points = dask_geopandas.from_geopandas(df_points, npartitions=4)
-
-#     ddf_result = ddf_points.spatial_shuffle(by="hilbert", calculate_partitions=False)
-#     result = (await c.compute(ddf_result)).val
-
-#     expected = df_points.sort_values("geometry").reset_index(drop=True)
-#     assert_geodataframe_equal(result.reset_index(drop=True), expected)
