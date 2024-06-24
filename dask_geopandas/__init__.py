1f232c39ff4121ce7615d550e0023a75a9ce20ed
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-06-24 18:18:23 +0200

    update versioneer config (v tag prefix) and regenerate versioneer files

--- dask_geopandas/__init__.py
+++ dask_geopandas/__init__.py
@@ -56,3 +56,7 @@ __version__ = _version.get_versions()["version"]
 from . import _version
 
 __version__ = _version.get_versions()["version"]
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
